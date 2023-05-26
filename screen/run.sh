2a182d4f99904f6f1cf6ac351dd97380f0133596
Einar Rosvoll <einar.rosvoll@gmail.com>
2023-05-26 09:10:15 +0200

    Update

--- screen/run.sh
+++ screen/run.sh
@@ -1,9 +1,10 @@
 #ffmpeg -framerate 1 -pattern_type glob -i '*.png' -c:v libx264 -pix_fmt yuv420p out.mp4
 
-exit
 HEI=0
-for x in $(ls | sort -n | grep ".png$"); do
+for x in $(ls | sort | grep ".png$"); do
 	NEW=$(printf "%03d\n" $HEI).png
 	HEI=$((HEI+1))
-	mv $x $NEW
+	if [[ $NEW != $x ]]; then
+		mv -v $x $NEW
+	fi
 done
